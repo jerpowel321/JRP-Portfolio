26c8a6921db9e6766635269060f4570576b903b8
Jennifer Powell <jerpowel321@gmail.com>
2020-07-04 18:28:03 -0700

    update links to resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -64,7 +64,7 @@ export default class NavBar extends React.Component {
 									<a href="https://www.linkedin.com/in/jenniferrpowell/" target="_blank" rel="noopener noreferrer">
 										<i className="fab fa-2x fa-linkedin hvr-grow" style={{ color: white, paddingRight: "12px" }}></i>
 									</a>
-									<a href="https://www.dropbox.com/s/dvx6ymfli11cprt/JP_Resume.pdf?dl=0" target="_blank" rel="noopener noreferrer">
+									<a href="jennifer-powell.io/images/JenniferPowell_Resume.pdf" target="_blank" rel="noopener noreferrer">
 										<i className="fas fa-file-pdf fa-2x ml hvr-grow nml" style={{ color: white, paddingRight: "12px" }}></i>
 									</a>
 								</Grid>
