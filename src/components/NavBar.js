9d6c18d211031d88c2410482640f91fd121412eb
Jennifer Powell <jerpowel321@gmail.com>
2022-03-07 17:45:22 -0800

    update styles and logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -27,67 +27,149 @@ export default class NavBar extends React.Component {
 	}
 	render() {
 		return (
-			<AppBar id="nav" style={{ backgroundColor: "black" }} position="sticky" >
-				<Toolbar>
-					<Grid container >
-						<Grid item xs={6}>
-							<Grid id="navigation" container justify="flex-start" style={{ fontWeight: "bold" }}>
-								<div id="name" >
-									<p style={{ paddingRight: "30px", fontFamily: "Lato, sans-serif", color: "#FFFFFF" }}>
-										Jennifer Powell
-              					</p>
-								</div>
-								<Grid item style={{ paddingTop: "30px", paddingRight: "20px", color: white }}>
-									<a style={{ color: white }} href="/about" rel="noopener noreferrer">About</a>
-								</Grid>
-								<Grid item style={{ paddingTop: "30px", paddingRight: "20px", color: white }}>
-									<a style={{ color: white }} href="/about/#values" rel="noopener noreferrer">Values</a>
-								</Grid>
-								<Grid item style={{ paddingTop: "30px", paddingRight: "20px", color: white }}>
-									<a style={{ color: white }} href="/about/#skills" rel="noopener noreferrer">Skills</a>
-								</Grid>
-								<Grid item style={{ paddingTop: "30px", paddingRight: "20px", color: white }}>
-									<a style={{ color: white }} href="/" rel="noopener noreferrer">Projects</a>
-								</Grid>
-							</Grid>
-						</Grid>
-
-						<Grid item xs={6}>
-							<Grid container justify="flex-end" style={{alignContent: "center", height:"78px"}} >
-								<a id="menu" ><i className="fas fa-2x fa-bars" style={{ color: white }} onClick={this.menuClick}>
-								</i>
-								</a>
-								<Grid item style={{  color: white,}}>
-									<a href="https://github.com/jerpowel321" target="_blank" rel="noopener noreferrer">
-										<i className="fab fa-2x fa-github hvr-grow" style={{ color: white, paddingRight: "12px" }}></i>
-									</a>
-									<a href="https://www.linkedin.com/in/jenniferrpowell/" target="_blank" rel="noopener noreferrer">
-										<i className="fab fa-2x fa-linkedin hvr-grow" style={{ color: white, paddingRight: "12px" }}></i>
-									</a>
-									<a href="/images/JenniferPowell_Resume.pdf" target="_blank" rel="noopener noreferrer">
-										<i className="fas fa-file-pdf fa-2x ml hvr-grow nml" style={{ color: white, paddingRight: "12px" }}></i>
-									</a>
-								</Grid>
-							</Grid>
-							<Grid container justify="center" >
-								<Grid item style={{  }}>
-									{this.state.menuOpen ? (
-										<div id="littleMenu">
-											<a style={{ color: white, paddingBottom: "15px" }} href="/about" rel="noopener noreferrer">About</a>
-											<a style={{ color: white, paddingBottom: "15px" }} href="/about/#values" rel="noopener noreferrer">Values</a>
-											<a style={{ color: white, paddingBottom: "15px" }} href="/about/#skills" rel="noopener noreferrer">Skills</a>
-											<a style={{ color: white, paddingBottom: "15px"  }} href="/" rel="noopener noreferrer">Projects</a>
-										</div>
-									) : null
-									}
-								</Grid>
-							</Grid>
-						</Grid>
-					</Grid>
-				</Toolbar>
-
-			</AppBar>
-		);
+      <AppBar id="nav" style={{ backgroundColor: "black" }} position="sticky">
+        <Toolbar>
+          <Grid container>
+            <Grid item xs={7} style={{ alignSelf: "center" }} className="hideOnMobile">
+              <Grid
+                id="navigation"
+                container
+                justify="flex-start"
+                alignItems="center"
+                style={{ fontWeight: "bold" }}
+              >
+                <div id="name" style={{ marginRight: "20px" }}>
+                  <img
+                    src="./images/turtle-logo.png"
+                    alt="Turtle"
+                    width="200px"
+                  />
+                </div>
+                <Grid item style={{ paddingRight: "20px", color: white }}>
+                  <a
+                    style={{ color: white }}
+                    href="/about"
+                    rel="noopener noreferrer"
+                  >
+                    About
+                  </a>
+                </Grid>
+                <Grid item style={{ paddingRight: "20px", color: white }}>
+                  <a
+                    style={{ color: white }}
+                    href="/about/#values"
+                    rel="noopener noreferrer"
+                  >
+                    Values
+                  </a>
+                </Grid>
+                <Grid item style={{ paddingRight: "20px", color: white }}>
+                  <a
+                    style={{ color: white }}
+                    href="/about/#skills"
+                    rel="noopener noreferrer"
+                  >
+                    Skills
+                  </a>
+                </Grid>
+                <Grid item style={{ paddingRight: "20px", color: white }}>
+                  <a
+                    style={{ color: white }}
+                    href="/"
+                    rel="noopener noreferrer"
+                  >
+                    Projects
+                  </a>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={5}>
+              <Grid
+                container
+                justify="flex-end"
+                style={{ alignContent: "center", height: "78px" }}
+              >
+                <Grid item style={{ color: white }}>
+                  <a
+                    href="https://github.com/jerpowel321"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i
+                      className="fab fa-2x fa-github hvr-grow"
+                      style={{ color: white, paddingRight: "12px" }}
+                    ></i>
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/jenniferrpowell/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i
+                      className="fab fa-2x fa-linkedin hvr-grow"
+                      style={{ color: white, paddingRight: "12px" }}
+                    ></i>
+                  </a>
+                  <a
+                    href="/images/JenniferPowell_Resume.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i
+                      className="fas fa-file-pdf fa-2x ml hvr-grow nml"
+                      style={{ color: white, paddingRight: "12px" }}
+                    ></i>
+                  </a>
+                </Grid>
+                <a id="menu">
+                  <i
+                    className="fas fa-2x fa-bars"
+                    style={{ color: white, cursor: "pointer" }}
+                    onClick={this.menuClick}
+                  ></i>
+                </a>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Toolbar>
+        <Grid container style={{justifyContent: "center"}}>
+          <Grid item style={{}}>
+            {this.state.menuOpen ? (
+              <div id="littleMenu">
+                <a
+                  style={{color: white, marginBottom: "15px" }}
+                  href="/about"
+                  rel="noopener noreferrer"
+                >
+                  About
+                </a>
+                <a
+                  style={{ color: white, marginBottom: "15px" }}
+                  href="/about/#values"
+                  rel="noopener noreferrer"
+                >
+                  Values
+                </a>
+                <a
+                  style={{ color: white, marginBottom: "15px" }}
+                  href="/about/#skills"
+                  rel="noopener noreferrer"
+                >
+                  Skills
+                </a>
+                <a
+                  style={{ color: white, marginBottom: "15px" }}
+                  href="/"
+                  rel="noopener noreferrer"
+                >
+                  Projects
+                </a>
+              </div>
+            ) : null}
+          </Grid>
+        </Grid>
+      </AppBar>
+    );
 	}
 };
 
